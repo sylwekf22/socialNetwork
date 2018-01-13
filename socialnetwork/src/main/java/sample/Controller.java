aa8c88e9d461d589d60c1f834a5e969bfe40905c
Aardvarky <kopko.mateusz@gmail.com>
2018-01-13 16:02:15 +0100

    Revert "Configure hibernate and create class entities."
    This reverts commit 289c34012929a830d7b9293510d822b2c3f8ddcf.

--- socialnetwork/src/main/java/sample/Controller.java
+++ socialnetwork/src/main/java/sample/Controller.java
@@ -3,16 +3,8 @@ package sample;
 import databasequeries.AuthorsNodes;
 import javafx.fxml.FXML;
 import config.DatabaseConnectionHandler;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
-import pojo.Authors;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Iterator;
-import java.util.List;
 
 public class Controller {
 
@@ -27,18 +19,6 @@ public class Controller {
         AuthorsNodes authorsNodes = new AuthorsNodes();
         Integer integer = authorsNodes.countAuthors();
         System.out.println(integer.intValue());
-
-        SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Authors.class).buildSessionFactory();
-        Session session = sessionFactory.openSession();
-        Query query = session.createQuery("FROM Authors");
-        List list = query.list();
-
-        Iterator iterator = list.iterator();
-
-        while(iterator.hasNext()){
-            Authors authors = (Authors) iterator.next();
-            System.out.println(authors.getId());
-        }
     }
 
     private Main main;
