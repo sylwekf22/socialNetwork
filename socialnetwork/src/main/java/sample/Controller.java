feb63a76200c3a58e55a6a9048ca3a46e6ac3c2a
Aardvarky <kopko.mateusz@gmail.com>
2018-01-14 22:00:16 +0100

    Revert "Done degree algorithm."
    This reverts commit 2133025b39f9c76d8b7fe9e32c965493fb3816d8.

--- socialnetwork/src/main/java/sample/Controller.java
+++ socialnetwork/src/main/java/sample/Controller.java
@@ -1,12 +1,7 @@
 package sample;
 
-import databasequeries.AuthorsTitlesHibernate;
-import databasequeries.AvgDegree;
-import databasequeries.TitlesHibernate;
 import javafx.fxml.FXML;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
 
 public class Controller {
 
@@ -18,9 +13,6 @@ public class Controller {
 
     @FXML
     private void initialize() throws SQLException {
-        AvgDegree avgDegree = new AvgDegree();
-        float v = avgDegree.countAvgDegree();
-        System.out.println(v);
     }
 
     private Main main;
