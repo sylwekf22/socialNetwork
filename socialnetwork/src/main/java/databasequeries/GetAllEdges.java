525e621bc65965b92dd113fbd97a88a7d21e485b
Aardvarky <kopko.mateusz@gmail.com>
2018-01-19 15:55:15 +0100

    Refactor second exercise.

--- socialnetwork/src/main/java/databasequeries/GetAllEdges.java
+++ socialnetwork/src/main/java/databasequeries/GetAllEdges.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 public class GetAllEdges {
 
     private final DatabaseConnectionHandler databaseConnectionHandler;
-
     private final String queryEdges = "Select SUM((a.liczba/2)*(a.liczba-1)) as krawedz  FROM (Select id_tytulu, count(id_autora) As liczba\n" +
             "from tab_lacz1 \n" +
             "group by id_tytulu) As a\n" +
@@ -23,15 +22,15 @@ public class GetAllEdges {
     }
 
     public Integer countAuthors() throws SQLException {
-        ResultSet numberOfNodes = databaseConnectionHandler.getData(queryEdges);
+        ResultSet numberOfEdges = databaseConnectionHandler.getData(queryEdges);
 
         // Setting cursor on the value
-        numberOfNodes.next();
-        Integer nodes = numberOfNodes.getInt("krawedz");
-        numberOfNodes.next();
-        nodes += numberOfNodes.getInt("krawedz");
-        databaseConnectionHandler.closeEnvironment();
+        numberOfEdges.next();
+        Integer edge = numberOfEdges.getInt("krawedz");
+        numberOfEdges.next();
+        edge += numberOfEdges.getInt("krawedz");
 
-        return nodes;
-}
+        databaseConnectionHandler.closeEnvironment();
+        return edge;
+    }
 }
